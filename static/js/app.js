8edac4ee7ce858eab4be2150484b7a7571f40896
dsfackrell <84728935+dsfackrell@users.noreply.github.com>
2024-06-24 00:34:53 -0500

    bubble Chart

--- static/js/app.js
+++ static/js/app.js
@@ -4,10 +4,14 @@ function buildMetadata(sample) {
 
     // get the metadata field
     let metaData = data.metadata
-    console.log(metaData)
+
+    //console.log("meta")
+    //console.log(metaData)
 
     // Filter the metadata for the object with the desired sample number
     let sampleMeta = metaData[sample.id]
+
+    //console.log(sample.id)
 
     // Use d3 to select the panel with id of `#sample-metadata`
     let metaPanel = d3.select("#sample-metadata")
@@ -26,19 +30,41 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let samples = data.samples
 
     // Filter the samples for the object with the desired sample number
-
+    let sampleIndex = samples.findIndex(x => x.id == sample)
+    //console.log(samples[sampleIndex])
+    let currentSample = samples[sampleIndex]
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otu_ids = currentSample.otu_ids
+    let otu_labels = currentSample.otu_labels
+    let sample_values = currentSample.sample_values
 
     // Build a Bubble Chart
+    let trace1 = {
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: 'markers',
+      marker: {
+        color: otu_ids,
+        size: sample_values
+      }
+    }
 
+    let bubbleData = [trace1];
+
+    let bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      showlegend: false,
+      height: 600,
+      width: 1200
+    }
 
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout)
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
 
@@ -73,7 +99,8 @@ function init() {
     firstSample = data.samples[0]
 
     // Build charts and metadata panel with the first sample
-    buildCharts(firstSample)
+
+    buildCharts(firstSample.id)
     buildMetadata(firstSample)
   });
 }
